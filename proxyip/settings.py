d21599c962e604e8a7b55942245219fceb105c09
shianon <340447244@qq.com>
2017-01-20 18:06:26 +0800

    修改为不需要代理文件也可以采集

--- proxyip/settings.py
+++ proxyip/settings.py
@@ -16,21 +16,21 @@ NEWSPIDER_MODULE = 'proxyip.spiders'
 #是否启动cookies
 COOKIES_ENABLED = False
 #log级别CRITICAL、 ERROR、WARNING、INFO、DEBUG
-LOG_LEVEL = "INFO"
+LOG_LEVEL = "DEBUG"
 #是否启用robots
 ROBOTSTXT_OBEY = False
 #对单个域名最大并发量
 CONCURRENT_REQUESTS_PER_DOMAIN = 16
 
 DOWNLOADER_MIDDLEWARES = {
-    'common.downloadermiddleware.httpproxy.RandomProxyMiddleware':100,
+    #'common.downloadermiddleware.httpproxy.RandomProxyMiddleware':100,
     'common.downloadermiddleware.useragent.RandomPCAgent': 543,
     'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
     'scrapy.downloadermiddlewares.retry.RetryMiddleware':None,
 }
 #RandomProxyMiddleware 参数
 #RETRY_TIMES和PROXY_USED_TIMES的比值由代理的质量决定的
-HTTPPROXY_FILE_PATH = u"G:/我的网站/微信/content/proxyip/data/valid_proxy.txt"
+HTTPPROXY_FILE_PATH = u"G:/git/proxy_scrapy/data/valid_proxy.txt"
 RETRY_TIMES = 4
 PROXY_USED_TIMES = 2
 RETRY_HTTP_CODES = [500, 503, 504, 599, 403]
